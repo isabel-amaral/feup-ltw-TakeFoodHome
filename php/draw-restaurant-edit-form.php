d88531b417042a07ad7518df3925f2d30a9e75e0
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-05-13 14:59:56 +0100

    php: page for restauarant to edit account details

---
+++ php/draw-restaurant-edit-form.php
@@ -0,0 +1,16 @@
+<?php
+    function drawRestaurantEditForm() { ?>
+        <main>
+            <section id="restaurantInfo">
+                <form>
+                    Restaurant's Name:<input type="text" , name="username">
+                    Email:<input type="email" , name="email">
+                    Phone Number: <input type="number" , name="phone">
+                    Address: <input type="text" , name="address">
+                    <input class="submit" type="submit" value="Submit">
+                </form>
+            </section>
+        </main>
+    <?php
+    }
+?>
