931ddbb23c0aef55d81a7eb811c7177b73e11c39
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-05-14 11:24:34 +0100

    php: page for restaurant's orders

---
+++ php/draw-restaurant-orders.php
@@ -0,0 +1,26 @@
+<?php
+    function drawRestaurantOrders() { ?>
+        <main>
+            <section id="ordersRestaurant">
+                <h2>Order Number</h2>
+                <h2>State</h2>
+                <p></p>
+
+                <p id="orderNumber">1</p>
+                <p>Ready</p>
+                <button class="button">Change State</button>
+                <p id="orderNumber">2</p>
+                <p>Received</p>
+                <button class="button">Change State</button>
+                <p id="orderNumber">3</p>
+                <p>Preparing</p>
+                <button class="button">Change State</button>
+                <p id="orderNumber">4</p>
+                <p>Ready</p>
+                <button class="button">Change State</button>
+                
+            </section>
+        </main>
+    <?php
+    }
+?>
