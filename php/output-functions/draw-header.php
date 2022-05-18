e8672b0b702eac4f9e8c81909ba581e93de06e2d
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-05-18 01:21:51 +0100

    Adjusted header for active session and added logout button css

--- php/output-functions/draw-header.php
+++ php/output-functions/draw-header.php
@@ -1,4 +1,28 @@
-<?php 
+<?php
+    function drawLoginForm() {
+        session_start();
+        if ($_SESSION['username'] === NULL) { ?>
+            <form action="../../php/actions/login.php" method="post">
+                <span class="label">Username:</span><input type="text" name="username">
+                <span class="label">Password:</span><input type="password" name="password">
+                <span id="login-buttons">
+                    <a href="">Register</a>
+                    <button type="submit">Login</button>
+                </span>
+            </form>
+        <?php
+        }
+        else { ?>
+            <section id="user-session">
+                <h3><a href=""><?=$_SESSION['name']?></a></h3>
+                <form action="../../php/actions/logout.php">
+                    <button type="submit">Logout</button>
+                </form>
+            </section>
+        <?php
+        }
+    }
+
     function drawHeader() { ?>
         <!DOCTYPE html>
         <html lang="en">
@@ -10,14 +34,7 @@
         <body>
         <header>
             <h1>Take Food Home</h1>
-            <form action="../../php/actions/login.php" method="post">
-                <span class="label">Username:</span><input type="text" name="username">
-                <span class="label">Password:</span><input type="password" name="password">
-                <span id="login-buttons">
-                    <a href="">Register</a>
-                    <button type="submit">Login</button>
-                </span>
-            </form>
+            <?php drawLoginForm(); ?>
         </header>
     <?php
     }
