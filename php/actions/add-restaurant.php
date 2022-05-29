e76fd9c35f1b4f8af7a4d477469089bc11ff87c0
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-05-29 15:55:24 +0100

    Fixed verification of owner being logged in before creating restaurant

--- php/actions/add-restaurant.php
+++ php/actions/add-restaurant.php
@@ -1,12 +1,15 @@
 <?php
     require_once('../../database/db-connection.php');
+    require_once('../../database/data-fetching/user.php');
     require_once('../../database/data-insertion/insert-new-restaurant.php');
 
     session_start();
     $db = getDatabaseConnection('../../database/restaurants.db');
     $user_info = getUserbyID($db, $_SESSION['userID']);
-    if ($user_info['isOwner'] === 0) {
-        header('Location: ../../user-info-page.php');
+    if ($_SESSION['userID'] === NULL) {
+        header('Location: ../../register-page.php');
+    } else if ($user_info['owner'] === 0) {
+        header('Location: ../../user-info-page.php');    
     }
 
     $name = $_POST['name'];
@@ -15,8 +18,7 @@
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
-    $ownerID = $_SESSION['userID']
 
-    addRestaurantToDatabase($db, $name, $description, $category, $email, $phone, $address, $ownerID);
+    addRestaurantToDatabase($db, $name, $description, $category, $email, $phone, $address, $_SESSION['userID']);
     header('Location: ../../user-info-page.php');
 ?>
