900d1a3362c6e5cc34e093e0b53f0d01df0bb831
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-05-29 13:29:14 +0100

    Implemented new restaurant creation

---
+++ database/data-insertion/insert-new-restaurant.php
@@ -0,0 +1,24 @@
+<?php
+    function addRestaurantToDatabase($db, $name, $description, $category, $email, $phone, $address, $ownerID) {
+        $stmt = $db->prepare(
+            'INSERT INTO Restaurant (name, description, category, email, phoneNumber, address, ownerID) Values(
+                :name,
+                :description,
+                :category,
+                :email,
+                :phone,
+                :address,
+                :ownerID,
+            )'
+        );
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description),
+        $stmt->bindParam(':category', $category),
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':phone', $phone);
+        $stmt->bindParam(':address', $address);
+        $stmt->bindParam(':ownerID', $isOwner);
+
+        $stmt->execute();
+    }
+?>
