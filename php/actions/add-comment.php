8b4a06cc48b8c5d681e88c55e08b56321c4853d8
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-12 22:52:00 +0100

    Verification if user ordered from a restaurant upon submitting a comment

--- php/actions/add-comment.php
+++ php/actions/add-comment.php
@@ -1,9 +1,13 @@
 <?php
     require_once('../../database/db-connection.php');
+    require_once('../../database/data-fetching/user-orders.php');
     require_once('../../database/data-insertion/insert-new-comment.php');
 
     session_start();
     $db = getDatabaseConnection('../../database/restaurants.db');
+    if (!checkIfOrderExists($db, $_GET['restaurantID'])) {
+        die(header('Location: ../../restaurant-page.php?id=' . $_GET['restaurantID']));
+    }     
 
     $comment = $_POST['comment'];
     $date = date('Y-m-d');
