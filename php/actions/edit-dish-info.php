12b119a81cbaa71c30f45457609cbdaf62fcf3f5
YTheNoobGoda <davidespinhamarques@gmail.com>
2022-06-13 19:02:05 +0100

    dish img are beeing upload need to change the file name

--- php/actions/edit-dish-info.php
+++ php/actions/edit-dish-info.php
@@ -24,10 +24,40 @@
     $category = $_POST['category'];
     $picture = $_POST['picture'];
 
+    $target_dir = "../../img/";
+    $target_file = $target_dir . basename($_FILES["picture"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     if ($picture === '') {
         $picture = $dish_info['picture'];
-    }
+        $uploadOk = 0;
+    }else{
+        $check = getimagesize($_FILES["picture"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".<br>";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
 
-    updateDishInfo($db, $_GET['dishID'], $name, $description, $price, $category, $picture);
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+    }
+    echo($uploadOk);
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+      // if everything is ok, try to upload file
+      } else {
+        if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+          echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+        } else {
+          echo "Sorry, there was an error uploading your file.";
+        }
+      }
+
+    updateDishInfo($db, $_GET['dishID'], $name, $description, $price, $category, $_FILES["picture"]["name"]);
     header('Location: ../../restaurant-page.php?id=' . $_GET['restaurantID']);
 ?>
