7b2506f6ab082a1cdb3496c1747791f4a82bd01d
YTheNoobGoda <davidespinhamarques@gmail.com>
2022-06-14 17:25:40 +0100

    fixed css of reviews

--- php/output-functions/restaurant-info/draw-reviews.php
+++ php/output-functions/restaurant-info/draw-reviews.php
@@ -35,9 +35,9 @@
                         } ?>
 
                         <?php if ($ownerID === $_SESSION['userID']) { ?>
-                            <article id="add-response">
+                            <article class="add-response">
                                 <header><h3>Add your response</h3></header>
-                                <form id="add-response-form" action="php/actions/add-response.php?reviewID=<?=$review['reviewID']?>&restaurantID=<?=$_GET['id']?>" method="post">
+                                <form class="add-response-form" action="php/actions/add-response.php?reviewID=<?=$review['reviewID']?>&restaurantID=<?=$_GET['id']?>" method="post">
                                     <textarea name="response" cols="30" rows="10"></textarea>
                                     <input class="submit" type="submit" value="Submit">
                                 </form>
