1fa3cce4f8a5f2ddc628b8a88ce846fb11c2f556
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-14 11:57:10 +0100

    Added section to personal page to display a user's favourite restaurants

--- php/output-functions/draw-user-info.php
+++ php/output-functions/draw-user-info.php
@@ -3,6 +3,7 @@
         require_once('database/db-connection.php');
         require_once('database/data-fetching/user.php');
         require_once('database/data-fetching/restaurants.php');
+        require_once('database/data-fetching/favourites.php');
         $db = getDatabaseConnection('database/restaurants.db');
         $user = getUserbyId($db,$_SESSION['userID']);
         ?>
@@ -21,7 +22,21 @@
                 <button class="button" id="user-order-button" type="button" onclick="location.href='../user-orders-page.php'">My orders</button>
             </section>
 
-            
+            <section id="favourite-restaurants">
+                <article>
+                    <h2>Your Favourite Restaurants</h2>
+                    <ul>
+                    <?php
+                    $favourites = getAllFavourites($db, $_SESSION['userID']);
+                    foreach ($favourites as $favourite) { ?>
+                        <li><a href="../restaurant-page.php?id=<?=$favourite['restaurantID']?>"><?=$favourite['name']?></a></li>
+                    <?php
+                    }
+                    ?>
+                    </ul>
+                </article>
+            </section>
+
             <?php 
                 if($user['owner'] === 1) { ?>
                     <section id="restaurants">
@@ -29,7 +44,7 @@
                             <h2>Your Restaurants</h2>
                             <ul>
                             <?php
-                            $restaurants = getRestaurantbyOwner($db,$_SESSION['userID']);
+                            $restaurants = getRestaurantbyOwner($db, $_SESSION['userID']);
                             foreach ($restaurants as $restaurant) { ?>
                                 <li><a href="../restaurant-page.php?id=<?=$restaurant['restaurantID']?>"><?=$restaurant['name']?></a></li>
                             <?php
