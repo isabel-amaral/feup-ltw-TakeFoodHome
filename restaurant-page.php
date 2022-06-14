3e5d6653342e9f7abfcab9107d4c9ce6e9d3216b
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-14 12:55:40 +0100

    Added separate function to draw cart

--- restaurant-page.php
+++ restaurant-page.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require_once('php/output-functions/user-info/draw-cart.php');
     require_once('php/output-functions/restaurant-info/draw-restaurant-info.php');
     require_once('php/output-functions/restaurant-info/draw-dishes.php');
     require_once('php/output-functions/restaurant-info/draw-reviews.php');
@@ -7,6 +8,7 @@
     require_once('php/output-functions/draw-footer.php');
 
     drawHeader();
+    outputCart();
     outputRestaurantInfo();
     outputDishes();
     outputReviews();
