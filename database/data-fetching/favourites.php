ff3cfe127af3394c07f5516de16dfe5366c67492
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-14 11:53:08 +0100

    Added query to fetch all of a user's favourite restaurants

--- database/data-fetching/favourites.php
+++ database/data-fetching/favourites.php
@@ -1,4 +1,17 @@
 <?php
+    function getAllFavourites($db, $userID) {
+        $stmt = $db->prepare(
+            'SELECT Restaurant.restaurantID, name
+            FROM Favourite JOIN Restaurant ON Favourite.restaurantID = Restaurant.restaurantID
+            WHERE userID = :userID
+            ORDER BY name'
+        );
+        $stmt->bindParam(':userID', $userID);
+        $stmt->execute();
+        $favourites = $stmt->fetchAll();
+        return $favourites;
+    }
+
     function checkIfFavourite($db, $userID, $restaurantID) {
         $stmt = $db->prepare(
             'SELECT * FROM Favourite
