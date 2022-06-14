fc8d94eff0398304af46cc2d8093fffb78141d8e
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-14 01:25:17 +0100

    Imported star icon

--- php/output-functions/draw-footer.php
+++ php/output-functions/draw-footer.php
@@ -2,6 +2,7 @@
     function drawFooter() { ?>
         <footer>
             <p>&#169; Take Food Home, 2022</p>
+            <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
         </footer>
     <?php
     }
